a2dee8db3bbdeed8cfc02bed8f809528af335592
sirfoga <sirfoga@protonmail.com>
2018-12-08 18:57:05 +0100

    add sha256

--- js/utils.js
+++ js/utils.js
@@ -50,3 +50,31 @@ function getMMSSString(seconds) {
 
 	return mString + ":" + sString;
 }
+
+function hex(buffer) {
+	let hexCodes = [];
+	let view = new DataView(buffer);
+	for (let i = 0; i < view.byteLength; i += 4) {
+		// Using getUint32 reduces the number of iterations needed (we process 4 bytes each time)
+		let value = view.getUint32(i);
+
+		// toString(16) will give the hex representation of the number without padding
+		let stringValue = value.toString(16);
+
+		// We use concatenation and slice for padding
+		let padding = '00000000';
+		let paddedValue = (padding + stringValue).slice(-padding.length);
+		hexCodes.push(paddedValue);
+	}
+
+	// Join all the hex strings into one
+	return hexCodes.join("");
+}
+
+function sha256(str) {
+	// We transform the string into an arraybuffer.
+	const buffer = new TextEncoder("utf-8").encode(str);
+	return crypto.subtle.digest("SHA-256", buffer).then(function (hash) {
+		return hex(hash);
+	});
+}
